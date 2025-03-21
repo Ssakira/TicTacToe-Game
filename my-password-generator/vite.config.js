531bbec19a5295fe20b0bfab28f8c9e19e449a65
Sakira <141208286+Ssakira@users.noreply.github.com>
2025-03-21 21:24:09 +0545

    Delete my-password-generator directory

--- my-password-generator/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-    root: "src", // Set src as the root directory
-    build: {
-        outDir: "../dist", // Ensure build files are placed in dist
-        emptyOutDir: true, // Clears dist before building
-    },
-});
